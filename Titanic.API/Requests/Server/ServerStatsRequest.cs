2842c61da06008227a287c928844c05188c050ed
Lekuru <contact@lekuru.xyz>
2026-01-05 00:12:11 +0100

    Add server stats request

---
+++ Titanic.API/Requests/Server/ServerStatsRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Titanic.API.Models;
+
+namespace Titanic.API.Requests
+{
+    public class ServerStatsRequest : APIRequest<ServerStatsModel>
+    {
+        protected override ServerStatsModel Execute(TitanicAPI api)
+        {
+            return api.Get<ServerStatsModel>("/stats");
+        }
+    }
+}
