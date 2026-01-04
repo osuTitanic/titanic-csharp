ae661141313670f30f9bb1fba5d142cf2409bc85
Lekuru <contact@lekuru.xyz>
2026-01-05 00:15:24 +0100

    Add notification & client status enums

---
+++ Titanic.API/Constants/ClientStatus.cs
@@ -0,0 +1,21 @@
+namespace Titanic.API.Constants
+{
+    public enum ClientStatus
+    {
+        Offline = -1,
+        Idle = 0,
+        Afk = 1,
+        Playing = 2,
+        Editing = 3,
+        Modding = 4,
+        Multiplayer = 5,
+        Watching = 6,
+        Unknown = 7,
+        Testing = 8,
+        Submitting = 9,
+        Paused = 10,
+        Lobby = 11,
+        Multiplaying = 12,
+        OsuDirect = 13
+    }
+}
---
+++ Titanic.API/Constants/NotificationType.cs
@@ -0,0 +1,12 @@
+namespace Titanic.API.Constants
+{
+    public enum NotificationType
+    {
+        Welcome = 0,
+        Achievement = 1,
+        Chat = 2,
+        Forum = 3,
+        Beatmaps = 4,
+        Security = 5
+    }
+}
