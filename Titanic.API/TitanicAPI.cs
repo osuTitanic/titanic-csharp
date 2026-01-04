3a5d961b908a73238f6261bf5d0d0d4691adeb04
Lekuru <contact@lekuru.xyz>
2026-01-04 22:22:50 +0100

    Add usage of `HttpClient` when using .NET Core

--- Titanic.API/TitanicAPI.cs
+++ Titanic.API/TitanicAPI.cs
@@ -7,20 +7,35 @@ using Newtonsoft.Json;
 using Titanic.API.Models;
 using Titanic.API.Requests;
 
+#if NETCOREAPP
+using System.Net.Http;
+using System.Text;
+#endif
+
 namespace Titanic.API
 {
     public class TitanicAPI
     {
+        private readonly string baseUrl;
+#if NETCOREAPP
+        private readonly HttpClient client;
+#else
         private readonly WebClient client;
-        public TokenModel Token;
+#endif
 
+        public TokenModel Token;
         public bool IsLoggedIn => Token != null;
         public bool IsTokenExpired => Token == null || DateTime.Now > Token.ExpiresAt;
 
         public TitanicAPI(string baseUrl = "https://api.titanic.sh")
         {
+            this.baseUrl = baseUrl.TrimEnd('/');
+#if NETCOREAPP
+            client = new HttpClient();
+#else
             client = new WebClient();
-            client.BaseAddress = baseUrl;
+            client.BaseAddress = this.baseUrl;
+#endif
         }
 
         public T Get<T>(string endpoint, Dictionary<string, string> headers = null)
@@ -30,7 +45,11 @@ namespace Titanic.API
             lock (client)
             {
                 PrepareRequest(headers);
+#if NETCOREAPP
+                string responseJson = client.GetStringAsync(baseUrl + endpoint).GetAwaiter().GetResult();
+#else
                 string responseJson = client.DownloadString(endpoint);
+#endif
                 return JsonConvert.DeserializeObject<T>(responseJson);
             }
         }
@@ -44,7 +63,13 @@ namespace Titanic.API
                 // NOTE: we skip token checking for the refresh endpoint to avoid infinite loops
                 PrepareRequest(headers, endpoint != "/account/refresh");
                 string json = JsonConvert.SerializeObject(data);
+#if NETCOREAPP
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(baseUrl + endpoint, content).GetAwaiter().GetResult();
+                string responseJson = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+#else
                 string responseJson = client.UploadString(endpoint, "POST", json);
+#endif
                 return JsonConvert.DeserializeObject<T>(responseJson);
             }
         }
@@ -57,7 +82,13 @@ namespace Titanic.API
             {
                 PrepareRequest(headers);
                 string json = JsonConvert.SerializeObject(data);
+#if NETCOREAPP
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PutAsync(baseUrl + endpoint, content).GetAwaiter().GetResult();
+                string responseJson = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+#else
                 string responseJson = client.UploadString(endpoint, "PUT", json);
+#endif
                 return JsonConvert.DeserializeObject<T>(responseJson);
             }
         }
@@ -71,6 +102,13 @@ namespace Titanic.API
                 PrepareRequest(headers);
                 string json = JsonConvert.SerializeObject(data);
 
+#if NETCOREAPP
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("PATCH"), baseUrl + endpoint);
+                request.Content = content;
+                HttpResponseMessage response = client.SendAsync(request).GetAwaiter().GetResult();
+                string responseJson = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+#else
                 // WebClient does not support Patch, so we need to build the request manually
                 Uri requestUrl = new Uri(new Uri(client.BaseAddress), endpoint);
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestUrl);
@@ -94,8 +132,11 @@ namespace Titanic.API
                 using (StreamReader reader = new StreamReader(response.GetResponseStream()))
                 {
                     string responseJson = reader.ReadToEnd();
-                    return JsonConvert.DeserializeObject<T>(responseJson);
+#endif
+                return JsonConvert.DeserializeObject<T>(responseJson);
+#if !NETCOREAPP
                 }
+#endif
             }
         }
 
@@ -106,7 +147,12 @@ namespace Titanic.API
             lock (client)
             {
                 PrepareRequest(headers);
+#if NETCOREAPP
+                HttpResponseMessage response = client.DeleteAsync(baseUrl + endpoint).GetAwaiter().GetResult();
+                string responseJson = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+#else
                 string responseJson = client.UploadString(endpoint, "DELETE", "");
+#endif
                 return JsonConvert.DeserializeObject<T>(responseJson);
             }
         }
@@ -116,6 +162,21 @@ namespace Titanic.API
             if (checkToken)
                 EnsureValidAccessToken();
 
+#if NETCOREAPP
+            client.DefaultRequestHeaders.Clear();
+
+            if (Token != null)
+                client.DefaultRequestHeaders.Authorization = 
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Token.AccessToken);
+
+            if (headers == null)
+                return;
+
+            foreach (KeyValuePair<string, string> header in headers)
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
+            }
+#else
             client.Headers.Clear();
 
             if (Token != null)
@@ -128,6 +189,7 @@ namespace Titanic.API
             {
                 client.Headers[header.Key] = header.Value;
             }
+#endif
         }
 
         public void EnsureValidAccessToken()
