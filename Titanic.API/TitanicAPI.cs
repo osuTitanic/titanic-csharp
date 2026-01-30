79ffa75d228c17c0008e4ef793323834d4959593
jvyden <jvyden@jvyden.xyz>
2026-01-29 23:05:10 -0500

    Trimming compatibility

--- Titanic.API/TitanicAPI.cs
+++ Titanic.API/TitanicAPI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Titanic.API.Http;
@@ -11,6 +12,12 @@ namespace Titanic.API
 {
     public class TitanicAPI : IDisposable
     {
+        #if NET10_0_OR_GREATER
+        private const DynamicallyAccessedMemberTypes types = DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.AllProperties;
+        #elif NET8_0_OR_GREATER
+        private const DynamicallyAccessedMemberTypes types = DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.NonPublicProperties;
+        #endif
+        
 #pragma warning disable CA1859
         private readonly IHttpInterface _http;
 #pragma warning restore CA1859
@@ -39,6 +46,13 @@ namespace Titanic.API
             this._http.AddDefaultHeader("User-Agent", userAgent);
         }
         
+#if NET8_0_OR_GREATER
+        [UnconditionalSuppressMessage(
+            "Trimming",
+            "IL2026",
+            Justification = "The contract resolver is only used within already trim-compatible methods."
+        )]
+#endif
         private static readonly JsonSerializerSettings _settings = new()
         {
             ContractResolver = new DefaultContractResolver
@@ -47,7 +61,21 @@ namespace Titanic.API
             }
         };
         
-        private T Send<T>(HttpMethodType methodType, string endpoint, object content = null, Dictionary<string, string> headers = null, bool checkToken = true)
+
+#if NET8_0_OR_GREATER
+        [UnconditionalSuppressMessage(
+            "Trimming",
+            "IL2026",
+            Justification = "The appropriate code is marked as untrimmed."
+        )]
+#endif
+        private T Send
+#if NET8_0_OR_GREATER            
+            <[DynamicallyAccessedMembers(types)] T>
+#else
+            <T>
+#endif
+            (HttpMethodType methodType, string endpoint, object content = null, Dictionary<string, string> headers = null, bool checkToken = true)
         {
             if (checkToken)
                 EnsureValidAccessToken();
@@ -74,32 +102,62 @@ namespace Titanic.API
             return obj;
         }
 
-        public T Get<T>(string endpoint, Dictionary<string, string> headers = null)
+        public T Get
+#if NET8_0_OR_GREATER            
+            <[DynamicallyAccessedMembers(types)] T>
+#else
+            <T>
+#endif
+            (string endpoint, Dictionary<string, string> headers = null)
         {
             Debug.Print("TitanicAPI: GET " + endpoint);
             return this.Send<T>(HttpMethodType.GET, endpoint, null, headers);
         }
 
-        public T Post<T>(string endpoint, object data, Dictionary<string, string> headers = null)
+        public T Post
+#if NET8_0_OR_GREATER            
+            <[DynamicallyAccessedMembers(types)] T>
+#else
+            <T>
+#endif
+            (string endpoint, object data, Dictionary<string, string> headers = null)
         {
             Debug.Print("TitanicAPI: POST " + endpoint);
             // NOTE: we skip token checking for the refresh endpoint to avoid infinite loops
             return this.Send<T>(HttpMethodType.POST, endpoint, data, headers, endpoint != "/account/refresh");
         }
 
-        public T Put<T>(string endpoint, object data, Dictionary<string, string> headers = null)
+        public T Put
+#if NET8_0_OR_GREATER            
+            <[DynamicallyAccessedMembers(types)] T>
+#else
+            <T>
+#endif
+            (string endpoint, object data, Dictionary<string, string> headers = null)
         {
             Debug.Print("TitanicAPI: PUT " + endpoint);
             return this.Send<T>(HttpMethodType.PUT, endpoint, data, headers);
         }
 
-        public T Patch<T>(string endpoint, object data, Dictionary<string, string> headers = null)
+        public T Patch
+#if NET8_0_OR_GREATER            
+            <[DynamicallyAccessedMembers(types)] T>
+#else
+            <T>
+#endif
+            (string endpoint, object data, Dictionary<string, string> headers = null)
         {
             Debug.Print("TitanicAPI: PATCH " + endpoint);
             return this.Send<T>(HttpMethodType.PATCH, endpoint, data, headers);
         }
 
-        public T Delete<T>(string endpoint, Dictionary<string, string> headers = null)
+        public T Delete
+#if NET8_0_OR_GREATER            
+            <[DynamicallyAccessedMembers(types)] T>
+#else
+            <T>
+#endif
+             (string endpoint, Dictionary<string, string> headers = null)
         {
             Debug.Print("TitanicAPI: DELETE " + endpoint);
             return this.Send<T>(HttpMethodType.PUT, endpoint, null, headers);
